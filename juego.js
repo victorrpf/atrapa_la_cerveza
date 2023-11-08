d7268425d975f0726f9c59a675419f8bd967fd9d
VictorP <94711342+victorrpf@users.noreply.github.com>
2023-11-09 00:15:26 +0100

    Actualizar juego.js

--- juego.js
+++ juego.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 200;
+canvas.width = 250;
 canvas.height = 300;
 
 const beerImg = new Image();
